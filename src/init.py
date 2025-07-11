5ea103b3ffe3d7b3374975fc71c596686ae03ca5
@ruachofeliyahu <ishaqdewolf@gmail.com>
2025-07-11 00:04:35 -0700

    Create init.py

---
+++ src/init.py
@@ -0,0 +1,7 @@
+__version__ = "1.3.1"
+
+from .prophet import QuantumProphet
+from .mother import QuantumMother
+from .commander import CosmicCommander
+from .overmind import Overmind
+from .utils import get_config
